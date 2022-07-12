01315ce99453ec63485956ce25ba4a1749befa91
Anna <snegina@yandex.ru>
2022-07-12 18:00:02 +0300

    first_release

---
+++ bot.py
@@ -0,0 +1,46 @@
+#библиотеки, которые загружаем из вне
+import telebot
+TOKEN = '5401407303:AAGfN0UnHquDmxeeP75F4MtWaPM__q0urag'
+
+from telebot import types
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def welcome(message):
+	sti = open('welcom.webp', 'rb')
+	bot.send_sticker(message.chat.id, sti)
+
+	#клавиатура
+	markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+	item1 = types.KeyboardButton("👉Follow my git")
+	item2 = types.KeyboardButton("✍️Write message")
+
+	markup.add(item1, item2)
+
+	bot.send_message(message.chat.id, "Привет, {0.first_name}!".format(message.from_user, bot.get_me()),
+		parse_mode='html', reply_markup=markup)
+
+#назначаем действие для клавиатуры
+@bot.message_handler(content_types=['text'])
+def lalala(message):
+	if message.chat.type == 'private':
+		if message.text == 'Follow my git':
+			bot.send_message(message.chat.id, 'https://github.com/snegina')
+		elif message.text == 'Write message':
+			bot.send_message(message.chat.id, 'http://t.me/snegina')
+		else:
+			bot.send_message(message.chat.id, 'Не знаю что ответить😢')
+
+
+bot.polling(none_stop=True)
+
+
+
+
+
+
+
+
+
+#https://core.telegram.org/bots/api#available-methods
